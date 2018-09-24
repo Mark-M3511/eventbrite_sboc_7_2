0acc53dfa92c55e7b6e71c5b5e43a1dc0e8d1318
Mark McFarlane <mark.mcfarlane@gmail.com>
2018-09-24 02:15:43 -0400

    Change List: 1 - Added language default constant 2 - Refactored code to use new constant instead of empty string constant

--- src/Helper/EBAttendees.php
+++ src/Helper/EBAttendees.php
@@ -180,7 +180,7 @@ class EBAttendees{
       $a->homePhone2 = $this->getAnswer($attendee['answers'], EBConsts::EBS_QA_117, EBConsts::EBS_EMPTY_STRING);
       $a->emailConsent = $this->getAnswer($attendee['answers'], EBConsts::EBS_QA_118, 0);
       $a->additionalInfo = $this->getAnswer($attendee['answers'], EBConsts::EBS_QA_119, EBConsts::EBS_EMPTY_STRING);
-      $a->language =  $this->getAnswer($attendee['answers'], EBConsts::EBS_QA_120, EBConsts::EBS_EMPTY_STRING);
+      $a->language = $this->getAnswer($attendee['answers'], EBConsts::EBS_QA_120, EBConsts::EBS_DEFAULT_LANGUAGE);
       
       $this->attendees[$a->attendeeId] = $a;
     }
